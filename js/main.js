718215afb9e9bffcbd7244cb817623a496f4ddba
Riccardo Belli <riccardo_belli@icloud.com>
2020-04-07 16:51:25 +0200

    Test JS

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Clicco invia
+// $(document).ready(function(){
+//
+//   $('fa-paper-plane').click(
+//     function() {
+//     $('.right-chatbox').append('<div class="message-row rightAlign"</div>');
+//     $('.message-row rightAlign').append('div class="message sent"></div>');
+//   });
+//
+//
+// }
