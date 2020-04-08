5e3b8666f85cc5c52dfa96bd66779abbcd44eb8b
Riccardo Belli <riccardo_belli@icloud.com>
2020-04-08 19:25:35 +0200

    KeyUp function

--- js/main.js
+++ js/main.js
@@ -1,11 +1,54 @@
-// Clicco invia
-// $(document).ready(function(){
-//
-//   $('fa-paper-plane').click(
-//     function() {
-//     $('.right-chatbox').append('<div class="message-row rightAlign"</div>');
-//     $('.message-row rightAlign').append('div class="message sent"></div>');
-//   });
-//
-//
-// }
+$(document).ready(function(){
+
+  $('.fa-paper-plane').click(
+    onMyMessage
+  );
+  $('.myMessage').keypress(function (e) {
+    var key = e.which;
+    if(key == 13){
+      onMyMessage();
+    }
+  });
+
+  $('.filter').keyup(
+    function() {
+      var userSearch = $('.filter').val();
+      var usersFound = users.filter(
+        function(user) {
+          return  $(user).find($('h2'))[0].innerHTML.includes(userSearch);
+        }
+      );
+      $(usersFound).each(
+      function(i, user) {
+        if (jQuery.inArray( user, users)) {
+        } else {
+          $(user).addClass('hide');
+        }
+      })
+    }
+  );
+
+  $('.chat').each(
+    function(i, chat) {
+      users.push(chat);
+    }
+  );
+
+});
+// Risposta utente
+function answer() {
+  $('.right-chatbox').append('<div class="message received">' + 'ok' + '</div>');
+};
+// Al click dell'invio
+function onMyMessage() {
+  var myMess = $('.myMessage').val();
+  if (myMess == "") {
+    alert("La tua text area è vuota");
+  } else {
+    $('.right-chatbox').append('<div class="message sent">' + myMess + '</div>');
+    $('.myMessage').val('');
+    setTimeout(answer, 1000);
+  }
+};
+
+var users = [];
