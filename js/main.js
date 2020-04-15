312b9fafef95660045a8bb9678e724029343699e
Riccardo Belli <riccardo_belli@icloud.com>
2020-04-15 15:52:54 +0200

    Fix

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
 
-  $('.fa-paper-plane').click(
-    onMyMessage
-  );
+  $('.fa-paper-plane').click(onMyMessage);
   $('.myMessage').keypress(function (e) {
     var key = e.which;
     if(key == 13){
@@ -12,43 +10,42 @@ $(document).ready(function(){
 
   $('.filter').keyup(
     function() {
-      var userSearch = $('.filter').val();
-      var usersFound = users.filter(
-        function(user) {
-          return  $(user).find($('h2'))[0].innerHTML.includes(userSearch);
-        }
-      );
-      $(usersFound).each(
-      function(i, user) {
-        if (jQuery.inArray( user, users)) {
+    var userSearch = $(this).val().toLowerCase();
+
+    $('.chat').each(
+      function () {
+        var users = $(this).find('.chat-name').text().toLowerCase();
+        if (users.includes(userSearch)) {
+          $(this).show();
         } else {
-          $(user).addClass('hide');
+          $(this).hide();
         }
-      })
-    }
-  );
+    });
+  });
 
-  $('.chat').each(
-    function(i, chat) {
-      users.push(chat);
-    }
-  );
-
+  $('.chat').click(
+    function() {
+      $('.chat').removeClass("selected");
+      $(this).addClass("selected");
+      var dataAttr = $(this).data("conversation");
+      $('.chat-right').removeClass("active");
+      $('.chat-right[data-conversation="' + dataAttr + '"]').addClass("active");
+  });
 });
+
 // Risposta utente
 function answer() {
-  $('.right-chatbox').append('<div class="message received">' + 'ok' + '</div>');
+  $('.chat-right.active').append('<div class="row"><div class="message received">OK!<span class="time">9:55</span></div></div>');
 };
+
 // Al click dell'invio
 function onMyMessage() {
   var myMess = $('.myMessage').val();
   if (myMess == "") {
     alert("La tua text area è vuota");
   } else {
-    $('.right-chatbox').append('<div class="message sent">' + myMess + '</div>');
+    $('.chat-right.active').append('<div class="row"><div class="message sent col-md-3 offset-md-9">' + myMess + '<span class="time">9:55</span>' + '</div></div>');
     $('.myMessage').val('');
     setTimeout(answer, 1000);
   }
 };
-
-var users = [];
